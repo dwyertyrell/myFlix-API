22061943e9300d38fc737aabb001f0e250c686e2
dwyertyrell <Dwyertyrell@gmail.com>
2024-11-17 15:28:39 +0000

    added morgan and nodemon modules to the project
    added the two built-in node modules, in the "devDependencies" sections of the "package.json" file,  and "package-lock.json" file
    
    deleted previous http requests, and imported the two newly added built-in modules via require() function.

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 const express = require('express'),
     morgan = require('morgan'),
-    bodyParser=require('body-parser');
+    bodyParser=require('body-parser'),
+    uuid = require('uuid');
 const app= express();
 
 app.use(morgan('common'));
 app.use(bodyParser.json())
 
-app.use(express.static('public'));
-
-
-
-app.get('/movies', (req,res,) => {
-    res.json({'No.1': 'Batman', 'No.2': 'Harry potter', 'No.3':'James Bond'});
-});
-
-app.get('/', (req,res,) => {
-    res.send('welcome to my app!');
-})
-
-app.use((err,req,res, next) => {
-    console.log(err.stack);
-    res.status(500).send('something broke!');
-});
 
 app.listen(8080, () => {
     console.log('your app is running on port 8080');
-})
+});
